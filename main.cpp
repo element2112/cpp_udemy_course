24762ad2e32a1439b9ed444a08bfa22c89255a14
Brittanie <bs009@live.com>
2020-04-08 11:49:31 -0400

    I/O

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+  int input;
+
+  std::cout << "Enter your favorite number between 1 and 100\n";
+
+  std::cin >> input;
+
+  std::cout << "You typed " << input << " as your favorite number" << std::endl;
+
+
+  return 0;
+}
