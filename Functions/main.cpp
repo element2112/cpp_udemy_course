719377781135e7ccf7455626bf91f64bfa58e5d3
B S <bs009@live.com>
2020-04-13 13:48:39 -0400

    added default arguments

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -5,12 +5,32 @@ using namespace std;
 const double pi = 3.14159;
 
 double calc_volume_cylinder(double, double);
-
 double calc_area_circle(double);
-
 void area_circle();
-
 void volume_cylinder();
+
+// using default arguments
+double calc_cost(double base_cost, double tax_rate = 0.06, double shipping = 3.50);
+
+
+
+int main()
+{
+//    area_circle();
+//    volume_cylinder();
+    double cost {0};
+    cost = calc_cost(100.0, 0.08, 4.25); // will use no defaults
+    cost = calc_cost(100.0, 0.08); // will use default shipping only
+    cost = calc_cost(200.0); // will use default tax and shipping
+    return 0;
+}
+
+double calc_cost(double base_cost, double tax_rate, double shipping)
+{
+    return base_cost += (base_cost * tax_rate) + shipping;
+}
+
+
 
 double calc_volume_cylinder(double radius, double height)
 {
@@ -39,11 +59,4 @@ void volume_cylinder()
     cout << "Enter the height of the cylinder: ";
     cin >> height;
     cout << "\nThe volume of a cylinder with radius " << radius << " and height " << height << " is " << calc_volume_cylinder(radius, height) << endl;
-}
-
-int main()
-{
-    area_circle();
-    volume_cylinder();
-    return 0;
 }
